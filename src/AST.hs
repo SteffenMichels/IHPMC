2eeaf3a852e28f826842f5925a17e7080fbc1426
Steffen Michels <SteffenMichels@users.noreply.github.com>
2015-06-17 08:55:52 +0000

    missing source files

---
+++ src/AST.hs
@@ -0,0 +1,44 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  AST
+-- Copyright   :
+-- License     :  AllRightsReserved
+--
+-- Maintainer  :
+-- Stability   :
+-- Portability :
+--
+-- |
+--
+-----------------------------------------------------------------------------
+
+module AST
+    ( AST
+    ) where
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+data AST = AST
+    { rFuncDefs :: Map RFuncLabel RFuncDef
+    , rules     :: Map PredicateLabel RuleBody
+    , query     :: PredicateLabel
+    } deriving (Show)
+
+type PredicateLabel  = String
+type RFuncLabel      = String
+
+data RFuncDef = Flip Rational deriving (Show)
+
+data RuleBody = RuleBody [RuleBodyElement] deriving (Show)
+
+data RuleBodyElement = UserPredicate PredicateLabel
+                     | BuildInPredicate BuildInPredicate
+                     deriving (Show)
+
+data BuildInPredicate = BoolEq (Expr Bool) (Expr Bool)
+                      deriving (Show)
+
+data Expr a where
+    BoolConstant :: Bool -> Expr Bool
+    UserRFunc    :: RFuncLabel -> Expr a -- type depends on user input, has to be typechecked at runtime
+deriving instance Show (Expr a)
---
+++ src/Parser.hs
@@ -0,0 +1,21 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  Parser
+-- Copyright   :
+-- License     :  AllRightsReserved
+--
+-- Maintainer  :
+-- Stability   :
+-- Portability :
+--
+-- |
+--
+-----------------------------------------------------------------------------
+
+module Parser
+    ( parse
+    ) where
+import AST
+
+parse :: String -> AST
+parse src = undefined
